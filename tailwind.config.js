1ea516ffe4f4967944d565f3eb4d683345634140
antor-rafi <rafiul.antor@gmail.com>
2024-11-17 12:35:32 +0600

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all src files
-    "./public/index.html",        // Include HTML entry points
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
+    "./index.html",              // Include the HTML entry point
   ],
   theme: {
     extend: {},
